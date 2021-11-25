66e8c9b336bbd84bf312af9438e585fb687d2d27
dulce valeria guzman taveras <Blastomm@hotmail.com>
2021-11-25 19:51:31 -0400

    almost done

--- src/component/AppDownloadInfo.tsx
+++ src/component/AppDownloadInfo.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import Button from '@mui/material/Button';
+import BG_App from '../assets/img/BG_App.png';
+import Google_Play from '../assets/img/Google_Play.png';
+import IOS from '../assets/img/IOS.png';
 
 const AppDownloadInfo = () => {
     return(
         <div className="class-AppDownloadInfo">
-        {/* <img src={} alt="IMG" /> */}
-            <h2>
+            <h1>
                 Downloads <br/>
-                <span>Our App Today</span>
-            </h2>
-            <Button variant="contained">Contained</Button>
-            <Button variant="contained">Contained</Button>
+                <strong>Our App Today</strong>
+            </h1>
+            <Button variant="text">
+                <img src={Google_Play} alt="IMG" />
+            </Button>
+            <Button variant="text">
+                <img src={IOS} alt="IMG" />
+            </Button>
+            <img src={BG_App} alt="IMG" />
         </div>
     )
 }
--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,9 +1,69 @@
 import React from "react";
+import IMG_LOGO from '../assets/img/logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import Box from "@mui/material/Box";
+import Input from "@mui/material/Input";
+import Button from "@mui/material/Button";
+import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
     return(
         <div className="class-Footer" id="lolo">
-Footer
+            <img src={IMG_LOGO} alt="IMG_LOGO" />
+            <div>
+                <h1>Address</h1>
+                <p>
+                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as It is a long established fact that a reader will be distracted by the readable c
+                </p>
+            </div>
+            <div>
+                <h1>Links</h1>
+                <ul>
+                <li>
+                    <a href="#lolo">Home</a>
+                </li>
+                <li>
+                    <a href="#lolo">Our Drivers</a>
+                </li>
+                <li>
+                    <a href="#lolo">Ride With Us</a>
+                </li>
+                <li>
+                    <a href="#lolo">App</a>
+                </li>
+                <li>
+                    <a href="#lolo">Contact Us</a>
+                </li>
+            </ul>
+            </div>
+            <div>
+                <h1>Follow Us</h1>
+                <ul>
+                    <li>
+                        {/* <FontAwesomeIcon icon={faFacebook} /> Call Classics Service */}
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <h1>Newsletter</h1>
+                <Box
+                    component="form"
+                    sx={{
+                    '& > :not(style)': { m: 1 },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                >
+                    <span>
+                        <span className="class-cicle"/>
+                        <Input placeholder="Enter your email" />
+                    </span>
+                    <Button variant="contained" className="class-button-black">
+                        <FontAwesomeIcon icon={faThumbsUp} className="class-icono-rotate-and-flip"/> Subscribe
+                    </Button>
+                </Box>
+            </div>
         </div>
     )
 }
