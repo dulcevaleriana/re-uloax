9042c14247b3a0387e422ac61c2e5dee2b77e32c
dulce-guzman-azumo <dulce.guzman@azumo.co>
2023-03-10 13:54:43 -0400

    quit extra properties

--- src/js/useMediaQuery.js
+++ src/js/useMediaQuery.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-export default function useMediaQuery(query:any) {
+export default function useMediaQuery(query) {
   const [matches, setMatches] = useState(false);
 
   useEffect(() => {
